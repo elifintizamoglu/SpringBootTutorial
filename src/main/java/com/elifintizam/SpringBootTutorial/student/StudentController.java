6900a9f6dad500d581cd7a684e28dfdf07b05111
Elif <elifintizam@gmail.com>
2023-08-10 19:53:01 +0300

    Delete Mapping

--- src/main/java/com/elifintizam/SpringBootTutorial/student/StudentController.java
+++ src/main/java/com/elifintizam/SpringBootTutorial/student/StudentController.java
@@ -17,13 +17,18 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
     @PostMapping
-    public void registerNewStudent(@RequestBody Student student){
+    public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 
 }
--- src/main/java/com/elifintizam/SpringBootTutorial/student/StudentService.java
+++ src/main/java/com/elifintizam/SpringBootTutorial/student/StudentService.java
@@ -20,11 +20,19 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
-    public void addNewStudent(Student student){
+    public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
-        if(studentOptional.isPresent()){
+        if (studentOptional.isPresent()) {
             throw new IllegalStateException("Email taken.");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with id " + studentId + " does not exists.");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
