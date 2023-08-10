24105a7ff2067d88b3f6c67e0c9bb1064e5f39a9
Elif <elifintizam@gmail.com>
2023-08-10 16:08:23 +0300

    Api Layer

---
+++ src/test/java/com/elifintizam/SpringBootTutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elifintizam.SpringBootTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
