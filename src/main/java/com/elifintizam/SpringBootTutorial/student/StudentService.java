ae33d8e756d327d4423a307b31ccd90662db5bf8
Elif <elifintizam@gmail.com>
2023-08-10 18:43:03 +0300

    JPA Repository - Access to db

--- src/main/java/com/elifintizam/SpringBootTutorial/student/StudentService.java
+++ src/main/java/com/elifintizam/SpringBootTutorial/student/StudentService.java
@@ -1,23 +1,21 @@
 package com.elifintizam.SpringBootTutorial.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Maria Hwan",
-                        "maria@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 7),
-                        23
-                )
-        );
+        return studentRepository.findAll();
     }
 }
