767500b9d01a655929968a3062c57d40e4dc025e
Elif <elifintizam@gmail.com>
2023-08-10 19:17:05 +0300

    Transient

--- src/main/java/com/elifintizam/SpringBootTutorial/student/StudentConfig.java
+++ src/main/java/com/elifintizam/SpringBootTutorial/student/StudentConfig.java
@@ -18,14 +18,12 @@ public class StudentConfig {
             Student maria = new Student(
                     "Maria Hwan",
                     "maria@gmail.com",
-                    LocalDate.of(2000, JANUARY, 7),
-                    23
+                    LocalDate.of(2003, JANUARY, 7)
             );
             Student john = new Student(
                     "John Hwan",
                     "john@gmail.com",
-                    LocalDate.of(2002, FEBRUARY, 5),
-                    21
+                    LocalDate.of(2002, FEBRUARY, 5)
             );
             repository.saveAll(List.of(maria,john));
         };
